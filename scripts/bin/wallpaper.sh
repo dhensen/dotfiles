46a2e249f8bbe3ca61c761f09d12e5073827e38f
Dino Hensen <dino.hensen@gmail.com>
2017-01-10 20:28:12 +0100

    fill and retain apsect ratio instead of scale

--- scripts/bin/wallpaper.sh
+++ scripts/bin/wallpaper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while true; do
-    find ~/Pictures/wallpapers -maxdepth 1 -type f \( -name '*.jpg' -o -name '*.png' \) -print0 | shuf -n1 -z | xargs -0 feh --bg-scale
+    find ~/Pictures/wallpapers -maxdepth 1 -type f \( -name '*.jpg' -o -name '*.png' \) -print0 | shuf -n1 -z | xargs -0 feh --bg-fill
     sleep 2m
 done
