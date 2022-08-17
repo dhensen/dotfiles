4cf879b9ca540c7de8e224d5159d1741836190d2
Dino Hensen <dino.hensen@gmail.com>
2022-08-17 23:43:27 +0200

    working changes

--- bspwm/.config/bspwm/barleduc.bspwm.sh
+++ bspwm/.config/bspwm/barleduc.bspwm.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
 # put machine dependent stuff here
 
+stalonetray &
+snixembed --fork
+
 pkill -x wallpaper.sh
 wallpaper.sh &
 
 #nm-applet &
 #picom -b
 #picom --experimental-backends --config /etc/xdg/picom.conf
-picom -b --backend xr_glx_hybrid --vsync --config /dev/null
-stalonetray &
-
+#picom -b --backend xr_glx_hybrid --vsync --config /dev/null
+#picom -b --experimental-backends --config /dev/null
+picom -b --experimental-backends --vsync --config /dev/null
 
 #redshift -m randr -r -l 52.0685:4.5094 -t 5700:2700 &
 redshift-gtk -m randr -r -l 52.0685:4.5094 -t 5700:2700 &
 
-xautolock -locker lock.sh -time 10 -notify 20 -notifier lock_notify &
+iwgtk -i &
+polkit-dumb-agent &
+
+#xautolock -locker lock.sh -time 10 -notify 20 -notifier lock_notify &
+init_lock &
+
 
