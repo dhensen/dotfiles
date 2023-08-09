70d672c978b39d5c1b89bb822a585ccd0f6f89d5
Dino Hensen <dino.hensen@gmail.com>
2023-08-09 19:35:43 +0200

    changes

---
+++ bspwm/.config/bspwm/t5ssd.bspwm.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# put machine dependent stuff here
+
+stalonetray &
+snixembed --fork
+
+pkill -x wallpaper.sh
+wallpaper.sh &
+
+#nm-applet &
+picom -b --vsync --config /dev/null
+
+#redshift -m randr -r -l 52.0685:4.5094 -t 5700:2700 &
+redshift-gtk -m randr -r -l 52.0685:4.5094 -t 5700:2700 &
+
+iwgtk -i &
+polkit-dumb-agent &
+
+#xautolock -locker lock.sh -time 10 -notify 20 -notifier lock_notify &
+init_lock &
+
+setxkbmap -layout us,us -variant ,alt-intl -option grp:shifts_toggle
+setxkbmap -option caps:escape
+
+fix-touchpad
+
